3cbc899c8376542eb94a65b6846c8bc7432a7f71
LEX1ER <119485544+LEX1ER@users.noreply.github.com>
2023-07-28 11:34:13 +0800

    Update routes.ts

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -15,7 +15,7 @@ const routes: RouteRecordRaw[] = [
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    redirect: '/resume/lexter',
   },
 ];
 
