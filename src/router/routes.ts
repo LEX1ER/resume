dd0e6881b8fc858ca8067722d2a9ad69df17d7eb
LEX1ER <119485544+LEX1ER@users.noreply.github.com>
2026-03-02 21:08:39 +0800

    Refactor resume data structure and update resume page components
    - Updated lexter-latest.json to include additional fields for skills and personal information.
    - Removed old resume-page.vue and created a new structure for resume pages under separate directories.
    - Modified routes.ts to accommodate new resume page structure.
    - Implemented new resume-page.vue for the first resume with enhanced data handling and presentation.
    - Added lexter.json as a new data source for the resume.

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,17 +1,26 @@
 import { RouteRecordRaw } from 'vue-router';
 
-const routes: RouteRecordRaw[] = [ 
+const routes: RouteRecordRaw[] = [
   {
-    path: '/resume/:id',
+    path: '/resume/1/:id',
     component: () => import('layouts/app-layout.vue'),
-    children: [{ path: '', component: () => import('pages/resume-page.vue') }],
+    children: [
+      { path: '', component: () => import('pages/1/resume-page.vue') },
+    ],
+  },
+  {
+    path: '/resume/2/:id',
+    component: () => import('layouts/app-layout.vue'),
+    children: [
+      { path: '', component: () => import('pages/2/resume-page.vue') },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    redirect: '/resume/lexter-latest',
+    redirect: '/resume/1/lexter-latest',
   },
 ];
 
